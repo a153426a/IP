ec3f2d758cab7f700e09752a7f8d4ab5921fcb4f
Zhichao Li <zl2511@imperial.ac.uk>
2014-02-11 20:06:07 +0000

    still fixing parser

--- JET/src/jet/Main.java
+++ JET/src/jet/Main.java
@@ -9,12 +9,13 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Lexer l = new Lexer(System.in);
 		parser p=new parser(l);
+		System.out.println(l.next_token());
+		System.out.println(l.next_token());
+		System.out.println(l.next_token());
         //LogicStatement s = (LogicStatement) p.parse().value;
         //String ss = p.parse().toString();
 		p.parse();
-		System.out.println(l.next_token());
-		System.out.println(l.next_token());
-		System.out.println(l.next_token());
+		
 
 	}
 
