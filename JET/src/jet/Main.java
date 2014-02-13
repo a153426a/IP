8e5eef77f3bf156dd9fd7a80600d520e793deee9
Zhichao Li <zl2511@imperial.ac.uk>
2014-02-13 14:20:08 +0000

    lexer done!

--- JET/src/jet/Main.java
+++ JET/src/jet/Main.java
@@ -1,5 +1,7 @@
 package jet;
 
+import java.io.FileReader;
+
 import lexerAndParser.Lexer;
 import lexerAndParser.parser;
 import ast.LogicStatement;
@@ -7,16 +9,16 @@ import ast.LogicStatement;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		Lexer l = new Lexer(System.in);
-		parser p=new parser(l);
-		System.out.println(l.next_token());
-		System.out.println(l.next_token());
-		System.out.println(l.next_token());
-        //LogicStatement s = (LogicStatement) p.parse().value;
-        //String ss = p.parse().toString();
-		p.parse();
+		FileReader fr = new FileReader("test.txt");
 		
-
+		Lexer l = new Lexer(fr);
+		parser p = new parser(l);
+        LogicStatement s = (LogicStatement) p.parse().value;
+        //String ss = p.parse().toString();
+		System.out.println(s);
+		
+		if (fr != null)
+			fr.close();
 	}
 
 }
